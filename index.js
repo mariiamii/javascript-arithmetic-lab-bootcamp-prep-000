5c6b938c69c4a78bc206ffb1256db1108d560270
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-08 19:54:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var a, b 
+var a = Math.floor(Math.random() * 1000)
+var b = Math.floor(Math.random() * 1000)
 
 function add(a, b) {
   1 + 80
 }
-function subtract(a, b) {
+function subtract(60, 40) {
   60 - 40
 }
-function multiply(a, b) {
+function multiply(2, 3.4) {
   2 * 3.4
 }
-function divide(a, b) {
+function divide(5.0, 2.5) {
   5.0 / 2.5
 }
+
